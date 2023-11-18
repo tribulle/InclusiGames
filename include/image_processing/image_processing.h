646bbcdeed5fff2a1b441df6c804c1df7173e2bc
Alexandre Lecomte <61118120+tribulle@users.noreply.github.com>
2023-11-18 12:24:15 +0100

    ImageProcessingLib

--- include/image_processing/image_processing.h
+++ include/image_processing/image_processing.h
@@ -22,7 +22,8 @@ limitations under the License.
 #include "tensorflow/lite/schema/schema_generated.h"
 
 // Globals, used for compatibility with Arduino-style sketches.
-namespace {
+namespace ImageProcessing{
+    
 tflite::ErrorReporter* error_reporter = nullptr;
 const tflite::Model* model = nullptr;
 tflite::MicroInterpreter* interpreter = nullptr;
@@ -32,4 +33,49 @@ int inference_count = 0;
 
 constexpr int kTensorArenaSize = 2000;
 uint8_t tensor_arena[kTensorArenaSize];
+
+void GetModel(const void *g_model){
+    model = tflite::GetModel(g_model);
+        if (model->version() != TFLITE_SCHEMA_VERSION) {
+        TF_LITE_REPORT_ERROR(error_reporter,
+                    "Model provided is schema version %d not equal "
+                    "to supported version %d.",
+                    model->version(), TFLITE_SCHEMA_VERSION);
+        return;
+    }
+}
+
+void InitializeInterpreter(){
+    // This pulls in all the operation implementations we need.
+    // NOLINTNEXTLINE(runtime-global-variables)
+    static tflite::AllOpsResolver resolver;
+
+    // Build an interpreter to run the model with.
+    static tflite::MicroInterpreter static_interpreter(
+        model, resolver, tensor_arena, kTensorArenaSize, error_reporter);
+    interpreter = &static_interpreter;
+}
+
+void AllocateMemory(){
+    // Allocate memory from the tensor_arena for the model's tensors.
+    TfLiteStatus allocate_status = interpreter->AllocateTensors();
+    if (allocate_status != kTfLiteOk) {
+        TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
+        return;
+    }
+}
+
+void InitializeModel(const void *g_model){
+
+    //previous functions
+    GetModel(g_model);
+    InitializeInterpreter();
+    AllocateMemory();
+
+    // Obtain pointers to the model's input and output tensors.
+    input = interpreter->input(0);
+    output = interpreter->output(0);
+
+}
+
 }  // namespace
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include "../include/image_processing/image_processing.h"
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  const void* g_model;
+  ImageProcessing::InitializeModel(g_model);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // input->data.int8[0] = x_quantized;
+  // Run inference, and report any error
+  TfLiteStatus invoke_status = ImageProcessing::interpreter->Invoke();
+  if (invoke_status != kTfLiteOk) {
+    TF_LITE_REPORT_ERROR(ImageProcessing::error_reporter, "Invoke failed");
+    return;
+  }
 }
